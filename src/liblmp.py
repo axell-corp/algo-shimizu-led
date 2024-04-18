1201747412ae9babc486fed9c43b918b02bbc4f3
清水 海大 <k-shimizu@axell.axell.co.jp>
2024-04-18 16:42:45 +0900

    update mov2layout

--- src/liblmp.py
+++ src/liblmp.py
@@ -1,4 +1,8 @@
 import pandas as pd
+import cv2
+import numpy as np
+
+led_kinds = ["_B", "_G", "_R", "_A"]
 
 def parse_lmp(layout_list: list[list[str]]) -> dict[str, pd.DataFrame]:
     idx = 0
@@ -26,3 +30,22 @@ def export_lmp6r(data: dict[str, pd.DataFrame]) -> str:
         ret += "\n"
     return ret
 
+def sample_led(pixel: any, postfix: str) -> int:
+    value = 0
+    if postfix == "_B":
+        value = pixel[0]
+    if postfix == "_G":
+        value = pixel[1]
+    if postfix == "_R":
+        value = pixel[2]
+    if postfix == "_A":
+        value = 255
+    return int(value)
+
+def mean_sample_led(frame: cv2.typing.MatLike, x: int, y: int, postfix: str) -> int:
+    pixel_delta = [(0, 0), (1, 0), (1, 1), (0, 1), (-1, 1), (-1, 0), (-1, -1), (0, -1), (1, -1)]
+    pixel = np.zeros((3))
+    for d in pixel_delta:
+        pixel += frame[y + d[0], x + d[1]]
+    pixel /= 9
+    return sample_led(pixel, postfix)
--- src/mov2layout.py
+++ src/mov2layout.py
@@ -5,8 +5,8 @@ import numpy as np
 
 import liblmp
 
-layout: str = "../layout/Unicorn.lmpl6r"
-empty_pattern: str = "../layout/Unicorn.lmp6r"
+layout: str = "../layout/Unicorn2.lmpl6r"
+empty_pattern: str = "../layout/Unicorn2.lmp6r"
 
 layout_file = open(layout, "r", encoding="shift-jis")
 empty_pattern_file = open(empty_pattern, "r", encoding="shift-jis")
@@ -23,9 +23,9 @@ pattern_data = liblmp.parse_lmp(empty_pattern_list)
 led_df = layout_data["LAMP"].merge(layout_data["KINDPOSTFIX"], on="Kind")
 
 pattern_data["PATTERN"].loc[0] = [0, "BASE", 0, 1000, 0, "", 60, -1, 500, "TRUE", "test pattern", "FALSE", 0]
-video = cv2.VideoCapture("../data/dataset.mp4")
-start_time: int = 75 * 1000
-end_time: int = 75 * 1000 + 1000
+video = cv2.VideoCapture("../data/10.mp4")
+start_time: int = 1
+end_time: int = 10 * 1000
 duration = end_time - start_time
 fps = 30
 for i, row in led_df.iterrows():
@@ -33,44 +33,25 @@ for i, row in led_df.iterrows():
     pattern_data["LAYER"].loc[i] = [i, 0, led_name, "", "TRUE", "FALSE"]
     pattern_data["CLIP"].loc[i] = [i * 2, i, 0, duration, duration, 2, ""]
 
-def sample_led(pixel: any, postfix: str) -> int:
-    value = 0
-    if postfix == "_B":
-        value = pixel[0]
-    if postfix == "_G":
-        value = pixel[1]
-    if postfix == "_R":
-        value = pixel[2]
-    if postfix == "_A":
-        value = 255
-    return int(value)
-
-def mean_sample_led(frame: cv2.typing.MatLike, x: int, y: int, postfix: str) -> int:
-    pixel_delta = [(0, 0), (1, 0), (1, 1), (0, 1), (-1, 1), (-1, 0), (-1, -1), (0, -1), (1, -1)]
-    pixel = np.zeros((3))
-    for d in pixel_delta:
-        pixel += frame[y + d[0], x + d[1]]
-    pixel /= 9
-    return sample_led(pixel, postfix)
-
 frame_idx = 0
 frame_len = int(duration * (fps / 1000) + 1)
 frame_duration = int(1000 / fps)
 clip_data = np.zeros((len(led_df), frame_len))
 
-pixel_max = np.zeros(4)
-pixel_min = np.ones(4) * 255
-
-led_kinds = ["_B", "_G", "_R", "_A"]
-for frame_idx in range(0, frame_len):
-    frame_time = frame_idx * frame_duration
-    video.set(cv2.CAP_PROP_POS_MSEC, start_time + frame_time)
-    _, frame = video.read()
-    for i in range(3):
-        for j, row in led_df[led_df["Postfix"] == led_kinds[i]].iterrows():
-            value = mean_sample_led(frame, int(row["X"]), int(row["Y"]), row["Postfix"])
-            pixel_max[i] = max(pixel_max[i], value)
-            pixel_min[i] = min(pixel_min[i], value)
+# pixel_max = np.zeros(4)
+# pixel_min = np.ones(4) * 255
+# 
+# for frame_idx in range(0, frame_len):
+#     frame_time = frame_idx * frame_duration
+#     video.set(cv2.CAP_PROP_POS_MSEC, start_time + frame_time)
+#     _, frame = video.read()
+#     for i in range(3):
+#         for j, row in led_df[led_df["Postfix"] == led_kinds[i]].iterrows():
+#             value = liblmp.mean_sample_led(frame, int(row["X"]), int(row["Y"]), row["Postfix"])
+#             pixel_max[i] = max(pixel_max[i], value)
+#             pixel_min[i] = min(pixel_min[i], value)
+pixel_max = np.ones(4) * 255
+pixel_min = np.zeros(4)
 
 pixel_coef = 1.0 / (pixel_max - pixel_min)
 pixel_coef[3] = 1.0 / 255
@@ -87,9 +68,10 @@ for frame_idx in range(0, frame_len):
     _, frame = video.read()
     
     def add_keyframe(i, row):
-        value = mean_sample_led(frame, int(row["X"]), int(row["Y"]), row["Postfix"])
-        led_idx = led_kinds.index(row["Postfix"])
+        value = liblmp.mean_sample_led(frame, int(row["X"]), int(row["Y"]), row["Postfix"])
+        led_idx = liblmp.led_kinds.index(row["Postfix"])
         value = int((value - pixel_min[led_idx]) * pixel_coef[led_idx] * 255)
+        value = max(min(value, 255), 0)
         pattern_data["CLIP"].iat[i, 6] += "{},{};".format(frame_time, value)
         clip_data[i][frame_idx] = value
 
